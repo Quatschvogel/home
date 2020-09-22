947d05a3c00f0babf34495239f663267359e332c
Kilian Hollmann <kilianhollmann@MacBook-Pro.fritz.box>
2020-09-22 21:50:11 +0200

    neu

--- src/components/technologien.svelte
+++ src/components/technologien.svelte
@@ -5,14 +5,6 @@
 	import { fade, fly } from 'svelte/transition';
 	import { onMount, onDestroy, afterUpdate } from 'svelte';
 	let tog = false;
-
-	afterUpdate(() => {
-			tog = true; 
-	});
-
-	onDestroy(() => {
-		tog = false;
-	});
 </script>
 
 <!--------------------------------------------------------------------------->
@@ -25,21 +17,19 @@
 <!--------------------------------------------------------------------------->
 <!----------------------- HTML / MARKUP DER COMPONENTE ---------------------->
 <!--------------------------------------------------------------------------->
-<div class="h-full">
-{#if tog}
 <div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-4 flex">	
 	<div class="block mx-auto w-full space-y-8">
-	<div class="bg-transparent text-center rounded-lg border-gray-900 border-2" in:fly="{{x: 2000, duration: 1000}}">
+	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400">
 		<p class="sm:text-3xl text-xl font-bold">Welche Technologien kommen zum Einsatz?</p>
 	</div>
-	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2" in:fly="{{y: -50, delay: 1100, duration: 1000}}">
+	{#if tog}
+	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2" in:fly="{{y: -50, duration: 1000}}">
 		<p class="sm:text-xl text-lg font-bold p-4">
 			Ich bin ein Frontend und Backend Softwareentwickler für Websiten und APP´s. Für ihren Erfolg setzte ich
 			auf moderne technologien um mit dem Puls der Zeit zu gehen. Dieser Einsatz ist auch für ihr Business
 			von Vorteil, da die Webseiten beispielsweise 100% SEO und 100% der möglichen Performance besitzen.
 		</p>
 	</div>
+	{/if}
 	</div>
-</div>
-{/if}
 </div>

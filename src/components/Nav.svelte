c0c1ec4ed15d45d6b901e112867c058c267ec164
Kilian Hollmann <kilianhollmann@MacBook-Pro.fritz.box>
2020-09-22 21:04:15 +0200

    neu

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,7 +5,8 @@
 	export let segment;
 	
 	// Scrolling position
-	let y;
+	let y = 0;
+
 
 </script>
 
@@ -30,12 +31,12 @@
 <!--------------------------------------------------------------------------->
 <svelte:window bind:scrollY={y}/>
 
-<header class="{y < 6 ? 'bg-transparent' : 'bg-gray-800 rounded-b-lg '}  items-center flex justify-between py-4 sticky top-0 z-10 transBackground sm:py-6">
-	<div class="{y < 6 ? 'text-black bg-red-500 hover:bg-red-700 ml-10' : 'text-white hover:bg-gray-800 ml-4'} text-lg sm:text-2xl font-semibold transPadding rounded-full px-4">
+<header class="{y < 8 ? 'bg-transparent' : 'bg-gray-800 rounded-b-lg '}  items-center flex justify-between py-4 sticky top-0 z-10 transBackground sm:py-6">
+	<div class="{y < 8 ? 'text-black bg-red-500 hover:bg-red-700 ml-10' : 'text-white hover:bg-gray-800 ml-4'} text-lg sm:text-2xl font-semibold transPadding rounded-full px-4">
 		Logo
 	</div>
-	<div class="{y < 6 ? 'space-x-5 sm:space-x-20 mr-4' : 'space-x-2 sm:space-x-10 mr-0'} transText text-lg px-4 sm:text-2xl">
-		<a class="{y < 6 ? 'text-black bg-red-500 hover:bg-red-700' : 'text-white hover:bg-gray-800'} rounded px-2 py-1 transText" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a>
-		<a class="{y < 6 ? 'text-black bg-red-500 hover:bg-red-700': 'text-white hover:bg-gray-800'} rounded px-2 py-1 transText" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">Kontakt</a>
+	<div class="{y < 8 ? 'space-x-5 sm:space-x-20 mr-4' : 'space-x-2 sm:space-x-10 mr-0'} transText text-lg px-4 sm:text-2xl">
+		<a class="{y < 8 ? 'text-black bg-red-500 hover:bg-red-700' : 'text-white hover:bg-gray-800'} rounded px-2 py-1 transText" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a>
+		<a class="{y < 8 ? 'text-black bg-red-500 hover:bg-red-700': 'text-white hover:bg-gray-800'} rounded px-2 py-1 transText" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">Kontakt</a>
 	</div>
 </header>

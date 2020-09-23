0643026d3853f21cbf7f8ac4d3e311d7e8ee6f14
Kilian Hollmann <kilianhollmann@MacBook-Pro.fritz.box>
2020-09-23 20:09:37 +0200

    neu

--- src/components/technologien.svelte
+++ src/components/technologien.svelte
@@ -5,10 +5,9 @@
 	import { fade, fly } from 'svelte/transition';
 	import {onMount, beforeUpdate, afterUpdate, onDestroy} from 'svelte';
 	let tog = false;
-	let a = 0;
 	afterUpdate(() => {
 		if (process.browser && window) {
-			a = 1;
+			tog = true;
 		}
 	});
 </script>
@@ -18,15 +17,32 @@
 <!--------------------------------------------------------------------------->
 <style>
 
-	.transition {
-		animation: flyin 4s ease forwards;
+	.transitionRL {
+		animation: flyinRL 1s ease forwards;
 	}
 
-	@keyframes flyin {
-		from {
+	.transitionLR {
+		animation: flyinLR 1s ease forwards;
+	}
+
+	@keyframes flyinRL {
+		0% {
+			transform: translateX(200%);
 			opacity: 0;
 		}
-		to {
+		100% {
+			transform: translateX(0%);
+			opacity: 1;
+		}
+	}
+
+	@keyframes flyinLR {
+		0% {
+			transform: translateY(-50%);
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0%);
 			opacity: 1;
 		}
 	}
@@ -35,15 +51,14 @@
 <!--------------------------------------------------------------------------->
 <!----------------------- HTML / MARKUP DER COMPONENTE ---------------------->
 <!--------------------------------------------------------------------------->
-{#if a >= 1}
-<div class="transition">Hallllllloooo</div>
-<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-4 flex" in:fly="{{y: -100, duration: 1000}}">	
+{#if tog == true}
+<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-4 flex">	
 	<div class="block mx-auto w-full space-y-8">
-	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400">
+	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400 transitionLR">
 		<p class="sm:text-3xl text-xl font-bold">Welche Technologien kommen zum Einsatz?</p>
 	</div>
 
-	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2" >
+	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2 transitionRL" >
 		<p class="sm:text-xl text-lg font-bold p-4">
 			Ich bin ein Frontend und Backend Softwareentwickler für Websiten und APP´s. Für ihren Erfolg setzte ich
 			auf moderne technologien um mit dem Puls der Zeit zu gehen. Dieser Einsatz ist auch für ihr Business
--- src/components/werBinIch.svelte
+++ src/components/werBinIch.svelte
@@ -6,10 +6,9 @@
 	import {onMount, beforeUpdate, afterUpdate, onDestroy} from 'svelte';
 	let tog = false;
 	let y;
-	let a = 0;
 	afterUpdate(() => {
 		if (process.browser && window) {
-			a = 1;
+			tog = true;
 		}
 	});
 
@@ -20,20 +19,48 @@
 <!---------------------- CSS / BOOTSTRAP DER COMPONENTE --------------------->
 <!--------------------------------------------------------------------------->
 <style>
+	.transitionRL {
+		animation: flyinRL 1s ease forwards;
+	}
 
+	.transitionLR {
+		animation: flyinLR 1s ease forwards;
+	}
+
+	@keyframes flyinRL {
+		0% {
+			transform: translateX(-200%);
+			opacity: 0;
+		}
+		100% {
+			transform: translateX(0%);
+			opacity: 1;
+		}
+	}
+
+	@keyframes flyinLR {
+		0% {
+			transform: translateY(-50%);
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0%);
+			opacity: 1;
+		}
+	}
 </style>
 
 <!--------------------------------------------------------------------------->
 <!----------------------- HTML / MARKUP DER COMPONENTE ---------------------->
 <!--------------------------------------------------------------------------->
-<svelte:window bind:scrollY={y}/>
-{#if a >= 1}
-<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-8 flex" in:fly="{{y: -100, duration: 1000}}">	
+
+{#if tog == true}
+<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-8 flex">	
 	<div class="block mx-auto w-full space-y-8">
-	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400" >
+	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400 transitionLR" >
 		<p class="sm:text-3xl text-xl font-bold">Wer Bin ich?</p>
 	</div>
-	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2">
+	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2 transitionRL">
 		<p class="sm:text-xl text-lg font-bold p-4">
 			Ich bin ein Frontend und Backend Softwareentwickler für Websiten und APP´s. Für ihren Erfolg setzte ich
 			auf moderne technologien um mit dem Puls der Zeit zu gehen. Dieser Einsatz ist auch für ihr Business

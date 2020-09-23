5a1eb5ab9f654c618e9aa8601ce155ba3509c106
Kilian Hollmann <kilianhollmann@MacBook-Pro.fritz.box>
2020-09-23 15:20:52 +0200

    neu

--- src/components/technologien.svelte
+++ src/components/technologien.svelte
@@ -17,19 +17,21 @@
 <!--------------------------------------------------------------------------->
 <!----------------------- HTML / MARKUP DER COMPONENTE ---------------------->
 <!--------------------------------------------------------------------------->
-<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-4 flex">	
+{#if a >= 1}
+<div class="py-12 sm:py-16 xl:py-20 xl:px-10 xl:w-5/6 px-4 w-11/12 rounded-lg bg-gray-200 mx-auto mt-4 flex" in:fly="{{y: -100, duration: 1000}}">	
 	<div class="block mx-auto w-full space-y-8">
 	<div on:click={() => {tog = !tog}} class="bg-transparent text-center rounded-lg border-gray-900 border-2 cursor-pointer hover:bg-gray-400">
 		<p class="sm:text-3xl text-xl font-bold">Welche Technologien kommen zum Einsatz?</p>
 	</div>
-	{#if tog}
-	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2" in:fly="{{y: -50, duration: 1000}}">
+
+	<div class="bg-transparent text-left rounded-lg border-gray-900 border-2" >
 		<p class="sm:text-xl text-lg font-bold p-4">
 			Ich bin ein Frontend und Backend Softwareentwickler für Websiten und APP´s. Für ihren Erfolg setzte ich
 			auf moderne technologien um mit dem Puls der Zeit zu gehen. Dieser Einsatz ist auch für ihr Business
 			von Vorteil, da die Webseiten beispielsweise 100% SEO und 100% der möglichen Performance besitzen.
 		</p>
 	</div>
-	{/if}
+
 	</div>
 </div>
+{/if}
